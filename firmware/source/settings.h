c1e6e542f57d2ba22cb8e874673cded6da0c45c1
Andrew Pullin <pullin@berkeley.edu>
2012-12-06 13:10:33 -0800

    Changed radio settings #error to just a #warning

--- firmware/source/settings.h
+++ firmware/source/settings.h
@@ -8,7 +8,7 @@
 #define __SETTINGS_H
 
 
-#error "REQUIRED: Review and set radio channel & network parameters in firmware/source/settings.h  , then comment out this line."
+#warning "REQUIRED: Review and set radio channel & network parameters in firmware/source/settings.h  , then comment out this line."
 /////// Radio settings ///////
 // Motile address ; all MRI robots were shipped with this configuration
 #define RADIO_CHANNEL		0x0e
